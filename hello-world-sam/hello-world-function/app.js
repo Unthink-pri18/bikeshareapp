0560a9466b92e25d210bfb8e51f2c57553b04bb3
Unthink-pri18 <157363325+Unthink-pri18@users.noreply.github.com>
2024-05-17 10:43:14 +0530

    added no code change trigger

--- hello-world-sam/hello-world-function/app.js
+++ hello-world-sam/hello-world-function/app.js
@@ -1,7 +1,7 @@
 exports.lambdaHandler = async (event, context) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello, World! Today'),
+        body: JSON.stringify('Hello, World! Today no change until done manually'),
         headers: {
             'Content-Type': 'application/json'
         }
